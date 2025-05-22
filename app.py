f4b927e55fdc0d653517a0b60cc72fb45983acc6
Jayanth-0407 <jayanth04072006@gmail.com>
2025-05-22 18:23:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ def predict():
     return render_template("index.html",prediction_text="the predicted crop is: {}".format(prediction))
 
 if __name__=="__main__":
-    flask_app.run(debug=True,port=2000)
+    flask_app.run(debug=True,port=21123)
